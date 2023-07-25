1481f0708e57373f4b783810458ed292d750a870
pankaj-404 <pankajbhayre@gmial.com>
2023-07-25 15:41:05 +0530

    first commit

---
+++ client/src/theme.ts
@@ -0,0 +1,139 @@
+export const tokens = {
+  grey: {
+    100: "#f0f0f3",
+    200: "#e1e2e7",
+    300: "#d1d3da",
+    400: "#c2c5ce",
+    500: "#b3b6c2",
+    600: "#8f929b",
+    700: "#6b6d74",
+    800: "#48494e",
+    900: "#242427",
+  },
+  primary: {
+    // light green
+    100: "#d0fcf4",
+    200: "#a0f9e9",
+    300: "#71f5de",
+    400: "#41f2d3",
+    500: "#12efc8",
+    600: "#0ebfa0",
+    700: "#0b8f78",
+    800: "#076050",
+    900: "#043028",
+  },
+  secondary: {
+    // yellow
+    100: "#fcf0dd",
+    200: "#fae1bb",
+    300: "#f7d299",
+    400: "#f5c377",
+    500: "#f2b455",
+    600: "#c29044",
+    700: "#916c33",
+    800: "#614822",
+    900: "#302411",
+  },
+  greenAccent: {
+    100: "#dbf5ee",
+    200: "#b7ebde",
+    300: "#94e2cd",
+    400: "#70d8bd",
+    500: "#4cceac",
+    600: "#3da58a",
+    700: "#2e7c67",
+    800: "#1e5245",
+    900: "#0f2922",
+  },
+  redAccent: {
+    100: "#f8dcdb",
+    200: "#f1b9b7",
+    300: "#e99592",
+    400: "#e2726e",
+    500: "#db4f4a",
+    600: "#af3f3b",
+    700: "#832f2c",
+    800: "#58201e",
+    900: "#2c100f",
+  },
+  blueAccent: {
+    100: "#e1e2fe",
+    200: "#c3c6fd",
+    300: "#a4a9fc",
+    400: "#868dfb",
+    500: "#6870fa",
+    600: "#535ac8",
+    700: "#3e4396",
+    800: "#2a2d64",
+    900: "#151632",
+  },
+  tertiary: {
+    // purple
+    500: "#8884d8",
+  },
+  background: {
+    light: "#2d2d34",
+    main: "#1f2026",
+  },
+};
+
+// mui theme settings
+export const themeSettings = {
+  palette: {
+    primary: {
+      ...tokens.primary,
+      main: tokens.primary[500],
+      light: tokens.primary[400],
+    },
+    secondary: {
+      ...tokens.secondary,
+      main: tokens.secondary[500],
+    },
+    tertiary: {
+      ...tokens.tertiary,
+    },
+    grey: {
+      ...tokens.grey,
+      main: tokens.grey[500],
+    },
+    background: {
+      default: tokens.background.main,
+      light: tokens.background.light,
+    },
+  },
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+    fontSize: 12,
+    h1: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 32,
+    },
+    h2: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 24,
+    },
+    h3: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 20,
+      fontWeight: 800,
+      color: tokens.grey[200],
+    },
+    h4: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 14,
+      fontWeight: 600,
+      color: tokens.grey[300],
+    },
+    h5: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 12,
+      fontWeight: 400,
+      color: tokens.grey[500],
+    },
+    h6: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 10,
+      color: tokens.grey[700],
+    },
+  },
+};
