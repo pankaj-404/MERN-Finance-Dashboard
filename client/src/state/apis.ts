df43bfa0c053c20ca4cbcf1f6fc53b758cacc62e
pankaj-404 <pankajbhayre@gmial.com>
2023-07-26 21:34:34 +0530

    create services and types

--- client/src/state/apis.ts
+++ client/src/state/apis.ts
@@ -1,15 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "mainApi",
-  tagTypes: ["Kpis"],
+  tagTypes: ["Kpis","Products","Transactions"],
   endpoints: (build) => ({
     getKpis: build.query({
       query: () => `kpi/kpis/`,
       providesTags: ["Kpis"],
     }),
+    getProducts: build.query({
+      query: () => `product/products`,
+      providesTags: ["Products"],
+    }),
+    getTransactions: build.query({
+      query: () => `/transaction/transactions`,
+      providesTags: ["Transactions"],
+    }),
   }),
 });
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery,useGetTransactionsQuery } = api;
